d0a4903541774348dab6afac15283d81f5af7013
ilead-cong <2119452560@qq.com>
2024-05-23 13:46:52 +0800

    version

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
-    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "License :: OSI Approved :: MIT License",
     "Intended Audience :: Science/Research",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
-    "Topic :: Scientific/Engineering :: oligo",
-    "Topic :: Scientific/Engineering :: probes",
+    "Topic :: Scientific/Engineering :: Information Analysis"
 ]
 
 keywords = [
